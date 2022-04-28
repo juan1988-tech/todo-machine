759b274be55b5a7d6b08c538147c124a77408b12
Juan Franco <jfrancoh5@gmail.com>
2022-04-27 19:47:03 -0500

    componentes con css

---
+++ src/TodoCounter.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TodoCounter.css';
+
+const TodoCounter = () =>{
+  return(
+    <h2 className='TodoCounter'>Has completado 2 de 3 TODO's</h2>
+  )
+}
+
+export default TodoCounter;
---
+++ src/TodoSearch.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TodoSearch.css';
+
+const TodoSearch = () =>{
+  return(
+    <input type="text" placeholder='todo' className='TodoSearch'/>
+  )
+}
+
+export default TodoSearch;
